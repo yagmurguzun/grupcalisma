5538c8ae0ecaf275e51eeb53aa9641c57aa2a646
Elif Atci <eelif0600@gmail.com>
2023-12-14 21:59:15 +0300

    Elif

--- src/OrtakAlan.java
+++ src/OrtakAlan.java
@@ -10,14 +10,15 @@ public class OrtakAlan {
 
         System.out.println("Mehmet'in alani");
 
+        /*
 
+        (Elif) Arkadaslar branch mainde degişiklik yapmayin lutfen !
+        (Elif) Tum degisiklikleri kendi branchinizde yaptiktan sonra "push" yapmadan once kontrol edin !!
 
-
+         */
 
         //Kullanicidan bir cumle ve bir harf alin, harf cumlede kullanilmissa kac kere
         //kullanildigini yazdirin, kullanilmadiysa “harf cumlede kullanilmamis” yazdirin.
-
-        Scanner scanner=new Scanner(System.in);
 
 
     }
