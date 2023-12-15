527d66ab96c8db73d510645bc9940b411edbccc1
yagmurguzun <yagmurguzun@gmail.com>
2023-12-15 15:13:00 +0200

    enes cihat

---
+++ src/Enes.java
@@ -0,0 +1,5 @@
+public class Enes {
+    public static void main(String[] args) {
+        System.out.println("Enes");
+    }
+}
--- src/OrtakAlan.java
+++ src/OrtakAlan.java
@@ -13,11 +13,14 @@ public class OrtakAlan {
 
         System.out.println("Mehmet'in alani");
 
+        System.out.println("Enes burada");
+
 
         //Kullanicidan bir cumle ve bir harf alin, harf cumlede kullanilmissa kac kere
         //kullanildigini yazdirin, kullanilmadiysa “harf cumlede kullanilmamis” yazdirin.
 
 
+        /*
         Scanner scanner = new Scanner(System.in);
         System.out.println("lutfen bir cumle ve bir harf giriniz");
         String girilencumle0 = scanner.nextLine();
@@ -39,6 +42,30 @@ public class OrtakAlan {
 
     }
 
+         */
 
-}
+
+         /*
+        Verilen String bir array’deki her bir elementi kontrol edip,
+        - Kelimenin uzunlugu cift sayi ise ilk yarisini
+        - Kelimenin uzunlugu tek sayi ise ortadaki harf dahil ikinci yarisini
+        yeni bir listeye ekleyip yazdirin.
+
+        String [] isimler={"metin","elif","enes","yagmur","ozan","ali","ismail"};
+         */
+        String [] isimler={"metin","elif","enes","yagmur","ozan","ali","ismail"};
+
+        List<String> isimlerList=new ArrayList<>();
+
+        for (String each:isimler) {
+            if (each.length()%2==0){
+                isimlerList.add(each.substring(0,each.length()/2));
+        }else {
+                isimlerList.add(each.substring(each.length()/2));
+            }
+
+        }
+        System.out.println(isimlerList);
+
+    }
 }
